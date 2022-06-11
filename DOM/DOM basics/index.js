26426ed9115f50abe8275bb86994a52f36239a45
Ivan Oyervides <ivanoyca@gmail.com>
2022-06-11 16:43:07 -0500

    DOM Basics

---
+++ DOM/DOM basics/index.js
@@ -0,0 +1,8 @@
+document.querySelector("h1").innerHTML = "GoodBye"
+document.getElementsByTagName("li")[1].style.color = "red"
+document.getElementById("third").innerHTML = "Ivan"
+document.querySelector(".list a").style.color = "green"
+document.querySelector("button").style.backgroundColor = "yellow"
+
+document.querySelector("h1").classList.add("huge")
+document.querySelector(".list a").setAttribute("href", "https://bing.com/")
