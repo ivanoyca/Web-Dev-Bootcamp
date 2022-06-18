6abb7acc539e7ce714ca0e23546ed1c505a85a1f
Ivan Oyervides <ivanoyca@gmail.com>
2022-06-18 16:41:35 -0500

    BMI Calculator App

--- NodeJS/Calculator/calculator.js
+++ NodeJS/Calculator/calculator.js
@@ -9,9 +9,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html'); //__dirname = returns path of the file.
 })
 
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+})
+
+app.post('/bmicalculator', (req, res) => {
+    weight = parseFloat(req.body.weight)
+    height = parseFloat(req.body.height)
+    result = weight / Math.pow(height, 2)
+    res.send(`Your BMI is ${result}`)
+})
+
 app.post('/', (req, res) => {
-    num1 = Number(req.body.num1) //Specify type of data. Default type is set to string.
-    num2 = Number(req.body.num2)
+    let num1 = Number(req.body.num1) //Specify type of data. Default type is set to string.
+    let num2 = Number(req.body.num2)
     result = num1 + num2
     res.send(`The result of the sum is equal to ${result}`)
 })
