77e1db5ed249bec072a4dc630bf9be49356f0231
Ivan Oyervides <ivanoyca@gmail.com>
2022-06-18 15:53:00 -0500

    Added Routes on Server

--- NodeJS/my-express-server/server.js
+++ NodeJS/my-express-server/server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello World!</h1>")
 })
 
+app.get('/contact', (req, res) => {
+    res.send('Contact Me At: ivanoyca@gmail.com')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This Web Site is property of Ivan Oyervides')
+})
+
+app.get('/hobbies', (req, res) => {
+    res.send('<ul><li>Programming</li><li>Watching Netflix</li><li>Tech</li></ul>')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
